37ee5375a7b5ed1d7ec4f8487e8e3d808582d33a
Syed Faateh Sultan Kazmi <54814772+faatehsultan@users.noreply.github.com>
2021-01-12 17:51:18 +0500

    Add my rainbow loading bar using CDN

--- assets/logic.js
+++ assets/logic.js
@@ -1,40 +1,42 @@
 var i = 0;
 const fileUrl = 'assets/code.txt';
-var txt ; fetch(fileUrl).then( r => r.text() ).then( t => txt=t )
+var txt;
+fetch(fileUrl).then(r => r.text()).then(t => txt = t)
 var lettersPerClick = 3;
 
-document.onkeypress=function(e)
-{
-    if(e.key=="~")
-    {
-        document.getElementById("grant").style.display="block";
+const loadingTime = 3;
+
+function hideLoading(label) {
+    setTimeout(() => {
+        document.querySelector(".rainbow-loading-container").innerHTML = "";
+        document.querySelector(label).style.display = "block";
+    }, loadingTime * 1000);
+}
+
+document.onkeypress = function (e) {
+    if (e.key == "~") {
+        document.getElementById("grant").style.display = "block";
+        document.querySelector(".access-grant-msg").style.display = "none";
         document.getElementById("code").innerHTML = ">   ";
-        i=0;
-    }
-    else  if(e.key=="!")
-    {
-        document.getElementById("deny").style.display="block";
+        document.querySelector(".rainbow-loading-container").style.display = "flex";
+        startLoadingBar(loadingTime);
+        hideLoading(".access-grant-msg");
+        i = 0;
+    } else if (e.key == "!") {
+        document.getElementById("deny").style.display = "block";
         document.getElementById("code").innerHTML = ">   ";
-        i=0;
-    }
-    else  if(e.key=="`")
-    {
-        document.getElementById("password").style.display="block";
-    }
-    else
-    {
-        if (i >= txt.length)
-        {
+        i = 0;
+    } else if (e.key == "`") {
+        document.getElementById("password").style.display = "block";
+    } else {
+        if (i >= txt.length) {
             i = 0;
         }
-        for (let count = 0; count < lettersPerClick; count++)
-        {
-            if(txt.charAt(i)!='\n')
-            {
+        for (let count = 0; count < lettersPerClick; count++) {
+            if (txt.charAt(i) != '\n') {
                 document.getElementById("code").innerHTML += txt.charAt(i);
                 i++;
-            }
-            else{
+            } else {
                 document.getElementById("code").innerHTML += "<br><br>>   ";
                 i++;
             }
@@ -42,19 +44,13 @@ document.onkeypress=function(e)
     }
 }
 
-function hide(id)
-{
-    if(id===1)
-    {
-        document.getElementById("grant").style.display="none";
-    }
-    else if(id===2)
-    {
-        document.getElementById("deny").style.display="none";
-    }
-    else if(id===3)
-    {
-        document.getElementById("password").style.display="none";
+function hide(id) {
+    if (id === 1) {
+        document.getElementById("grant").style.display = "none";
+    } else if (id === 2) {
+        document.getElementById("deny").style.display = "none";
+    } else if (id === 3) {
+        document.getElementById("password").style.display = "none";
         document.getElementById("code").innerHTML += "<br><br>----> Login Successfully<br><br>>   ";
     }
 }
